c929b8e209ae582c85954aa72c82f3388713c9ad
tom <tjp.salet@gmail.com>
2026-02-23 08:07:00 +0100

    Fix of null value

--- dist/card.js
+++ dist/card.js
@@ -90,7 +90,8 @@ function toLatLon(point) {
 async function fetchHistory(hass, entityId, date) {
     const states = await fetchEntityHistory(hass, entityId, date);
     return states
-        .map((state) => toPoint(state));
+        .map((state) => toPoint(state))
+        .filter(Boolean);
 }
 
 async function fetchEntityHistory(hass, entityId, date) {
